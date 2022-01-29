c0ed9f7aa38d04eee8547323215bb29d399c84e7
astsu <you@example.com>
2022-01-29 21:53:40 +0200

    feat: Add: `role` field to `UserInRepo` model; `UserWithRoleAdd` model.

--- tvsched/application/models/auth.py
+++ tvsched/application/models/auth.py
@@ -1,12 +1,23 @@
 from dataclasses import dataclass
 
+from tvsched.entities.auth import Role
+
 
 @dataclass(frozen=True)
 class UserAdd:
-    """Data for register user to repo."""
+    """Data for register user with USER role."""
 
     username: str
     password: str
+
+
+@dataclass(frozen=True)
+class UserWithRoleAdd:
+    """Data for register user with specific role."""
+
+    username: str
+    password: str
+    role: Role
 
 
 @dataclass(frozen=True)
@@ -15,3 +26,4 @@ class UserInRepo:
 
     username: str
     password_hash: str
+    role: Role
