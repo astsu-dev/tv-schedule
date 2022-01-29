f11a17c384913f25685fb5787eccef460d03cbba
astsu <you@example.com>
2022-01-29 21:55:30 +0200

    feat: Now `AddUserUseCase` adds user with USER role.

--- tvsched/application/use_cases/auth/add_user_use_case.py
+++ tvsched/application/use_cases/auth/add_user_use_case.py
@@ -1,34 +1,22 @@
-from typing import Protocol
-
 from tvsched.application.interfaces import ILogger
-from tvsched.application.exceptions.auth import UserAlreadyExistsError
-from tvsched.application.models.auth import UserAdd, UserInRepo
-from tvsched.application.utils.auth import hash_password
-
-
-class IAddUserUseCaseRepo(Protocol):
-    """"""
-
-    async def add_user(self, user: UserInRepo) -> None:
-        """Adds user to repo.
-
-        Args:
-            user (UserInRepo): data for adding user to repo
-
-        Raises:
-            UserAlreadyExistsError: will be raised when user already exists
-        """
+from tvsched.application.models.auth import UserAdd, UserWithRoleAdd
+from tvsched.application.use_cases.auth.add_user_with_role_use_case import (
+    AddUserWithRoleUseCase,
+    IAddUserWithRoleUseCaseRepo,
+)
+from tvsched.entities.auth import Role
 
 
 class AddUserUseCase:
-    """Adds user to repo"""
+    """Adds user with USER role to repo."""
 
-    def __init__(self, repo: IAddUserUseCaseRepo, logger: ILogger) -> None:
+    def __init__(self, repo: IAddUserWithRoleUseCaseRepo, logger: ILogger) -> None:
         self._repo = repo
         self._logger = logger
+        self._use_case = AddUserWithRoleUseCase(repo, logger)
 
     async def execute(self, user: UserAdd) -> None:
-        """Adds user to repo.
+        """Adds user with USER role to repo.
 
         Args:
             user (UserAdd): data for adding user to repo
@@ -37,20 +25,5 @@ class AddUserUseCase:
             UserAlreadyExistsError: will be raised when user already exists
         """
 
-        logger = self._logger
-
-        logger.info(f"Start adding user {user.username} to repo")
-
-        username = user.username
-
-        password_hash = hash_password(user.password)
-
-        user_in_repo = UserInRepo(username=user.username, password_hash=password_hash)
-
-        try:
-            await self._repo.add_user(user_in_repo)
-        except UserAlreadyExistsError:
-            logger.info(f"User with name {username} already exists.")
-            raise
-
-        logger.info(f"Finish adding user {user.username} to repo")
+        user_with_role = UserWithRoleAdd(user.username, user.password, Role.USER)
+        await self._use_case.execute(user_with_role)
